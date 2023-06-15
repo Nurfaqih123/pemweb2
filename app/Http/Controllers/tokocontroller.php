0fbaa5d30f3a214529deed91898afc403fff9375
Nurfaqih123 <muhammadnurfaqih941@gmail.com>
2023-06-15 12:10:51 +0700

    -

---
+++ app/Http/Controllers/tokocontroller.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class tokocontroller extends Controller
+{
+    public function index()
+    {
+        return view('toko/index');
+    }
+
+    public function detail(){
+        return view('toko/detail');
+    }
+    
+    public function about(){
+        return view('toko/about');
+    }
+
+    public function form(){
+        return view('toko/form');
+    }
+
+    public function profile(){
+        return view('toko/profie');
+    }
+
+    public function admin(){
+        $products = Product::all();
+        return view('toko/admin', compact('products'));
+    }
+
+    public function create(){
+        return view('toko/create');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required',
+            'description' => 'required'
+        ]);
+
+        Product::create($request->all());
+        return redirect()->route('product.admin')->with('success','Product created successfully');
+        
+    }
+}
---
+++ resources/views/toko/about.blade.php
@@ -0,0 +1,49 @@
+@extends('template.main')
+
+@section('content')
+    <div class="row my-7">
+        <div class="col-md-3">
+            <img src="/img/food.jpg" alt="" class="shadow-lg w-100 rounded" />
+        </div>
+        <div class="col-md-7">
+            <h1 class="display-5 fw-bold">Welcome to F-STORE!</h1>
+            <p class="me-4">
+            Makanan adalah salah satu kenikmatan hidup yang tidak dapat disangkal. 
+            Setiap hidangan memiliki karakteristik uniknya sendiri, mulai dari rasa yang lezat hingga tampilan yang menggugah selera. 
+            Dalam dekripsi ini, kita akan menjelajahi dunia makanan dan menggambarkan kelezatan yang dapat kita temukan dalam setiap gigitan.
+
+            Inilah saatnya untuk menikmati pengalaman kuliner yang tak terlupakan! Kami dengan bangga mempersembahkan promosi makanan yang menggugah selera,
+            di mana Anda dapat mengeksplorasi kelezatan hidangan terbaik yang siap memanjakan lidah Anda. Bersiaplah untuk mencicipi cita rasa yang menggoda dan menjalani petualangan kuliner yang tak terlupakan!
+               
+            Kami memberikan perhatian penuh pada kualitas hidangan yang kami tawarkan. Setiap hidangan dipersiapkan dengan menggunakan bahan-bahan segar dan
+            berkualitas tinggi untuk memberikan rasa dan tekstur yang sempurna. Dengan perpaduan bumbu yang tepat dan teknik memasak yang ahli, hidangan kami menawarkan cita rasa yang otentik dan memukau.
+                
+            </p>
+        </div>
+    </div>
+@endsection
+
+@section('content2')
+<div class="row my-7">
+        <div class="col-md-4">
+            <img src="/img/food2.jpg" alt="" class="shadow-lg w-100 rounded" />
+        </div>
+        <div class="col-md-7">
+            <p class="me-4">
+            Pengalaman Kuliner yang Tak Terlupakan
+            Promosi makanan ini bukan hanya tentang makanan lezat, tetapi juga tentang menciptakan pengalaman kuliner yang tak terlupakan.
+            Nikmati makanan yang disajikan dengan penuh keindahan dan tata letak yang menawan. Apakah Anda merayakan momen spesial, 
+            mengadakan pertemuan bisnis, atau hanya ingin menikmati waktu bersantai dengan keluarga dan teman, kami siap memberikan pengalaman
+            kuliner yang memuaskan dan mengesankan.
+
+            Jangan lewatkan kesempatan untuk menjelajahi kelezatan kuliner terbaik! Segera manfaatkan promosi
+                
+            </p>
+        </div>
+        <div class="col-md-6">
+            <img src="/img/img5.jpg" alt="" class="shadow-lg w-100 rounded" />
+        </div>
+    </div>
+@endsection
+
+    
